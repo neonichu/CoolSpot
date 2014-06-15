92550b670c4ea5634917e12514607854161416f1
Boris Bügling <boris@icculus.org>
2014-06-15 22:09:28 +0200

    Working authentication for Spotify.

--- Code/AppDelegate.swift
+++ Code/AppDelegate.swift
@@ -8,42 +8,70 @@
 
 import UIKit
 
+let kClientId       = "spotify-ios-sdk-beta"
+let kCallbackURL    = "spotify-ios-sdk-beta://callback"
+
+let kSessionUserDefaultsKey = "SpotifySession"
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        var plistRep : AnyObject! = NSUserDefaults.standardUserDefaults().valueForKey(kSessionUserDefaultsKey)
+        var session = SPTSession(propertyListRepresentation:plistRep)
+        
+        if (session.credential? && countElements(String(session.credential)) > 0) {
+            self.enableAudioPlaybackWithSession(session)
+        } else {
+            var auth = SPTAuth.defaultInstance()
+            var loginURL = auth.loginURLForClientId(kClientId,
+                declaredRedirectURL:NSURL.URLWithString(kCallbackURL), scopes:["login"])
+            
+            var delayInSeconds = 0.1
+            var popTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Double(NSEC_PER_SEC)))
+            dispatch_after(popTime, dispatch_get_main_queue(), {
+                UIApplication.sharedApplication().openURL(loginURL)
+                return
+            })
+        }
+        
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        // Override point for customization after application launch.
         self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.rootViewController = UIViewController()
         self.window!.makeKeyAndVisible()
+        
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    func application(application: UIApplication!, openURL url: NSURL!, sourceApplication: String!, annotation: AnyObject!) -> Bool {
+        
+        var authCallback = { (error: NSError!, session: SPTSession!) -> Void in
+            if (error != nil) {
+                NSLog("*** Auth error: %@", error)
+                return
+            }
+            
+            NSUserDefaults.standardUserDefaults().setValue(session.propertyListRepresentation(),
+                forKey:kSessionUserDefaultsKey)
+            self.enableAudioPlaybackWithSession(session)
+        }
+        
+        if (SPTAuth.defaultInstance().canHandleURL(url, withDeclaredRedirectURL:NSURL.URLWithString(kCallbackURL))) {
+            SPTAuth.defaultInstance().handleAuthCallbackWithTriggeredAuthURL(url,
+                tokenSwapServiceEndpointAtURL:NSURL.URLWithString("http://localhost:1234/swap"),
+                callback:authCallback)
+            return true
+        }
+        
+        return false
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func enableAudioPlaybackWithSession(session: SPTSession!) {
+        //var viewController = self.window!.rootViewController
+        //viewController.handleNewSession(session)
     }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
 
--- Podfile
+++ Podfile
@@ -2,8 +2,8 @@ platform :ios, "8.0"
 
 target "CoolSpot" do
 
-#pod 'SpotifySDK', :git => 'https://github.com/neonichu/ios-sdk'
-pod 'SpotifySDK', :path => '../../Sources/spotify-ios-sdk'
+pod 'SpotifySDK', :git => 'https://github.com/neonichu/ios-sdk'
+#pod 'SpotifySDK', :path => '../../Sources/spotify-ios-sdk'
 
 end
 
